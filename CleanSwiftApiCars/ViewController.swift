c19f2f875a12bf5b554dda748a9f0bcf18e6285b
andrebrilho <andre.txt@hotmail.com>
2019-09-12 22:08:28 -0300

    Initial Commit

---
+++ CleanSwiftApiCars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanSwiftApiCars
+//
+//  Created by André Brilho on 12/09/19.
+//  Copyright © 2019 André Brilho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
